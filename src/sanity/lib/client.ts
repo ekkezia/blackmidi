f0979953da4500219d09ce8577a46a22941c900f
ekezia <e.kezia@gmail.com>
2025-07-23 02:16:32 +0700

    update env

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -5,7 +5,7 @@ import { createClient } from 'next-sanity'
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION || '2023-10-16', // use a UTC date string
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-10-16', // use a UTC date string
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
 
