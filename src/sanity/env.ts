a9a1ad2cb511eb31d41d9d344daaecb926cb9a22
ekezia <e.kezia@gmail.com>
2025-07-22 19:19:24 +0700

    chore: env

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,14 +2,12 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-07-21'
 
 export const dataset = assertValue(
-  // process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'production',
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
   'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
 )
 
 export const projectId = assertValue(
-  // process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'n33clf8c',
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,9 +3,9 @@ import { createClient } from 'next-sanity'
 // import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId: 'n33clf8c',
-  dataset: 'production',
-  apiVersion: '2025-07-21',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.SANITY_API_VERSION || '2023-10-16', // use a UTC date string
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
 
